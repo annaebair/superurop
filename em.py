4fa8904b71a99d69b7717c9c25b7803b3492d305
Anna Bair <annabair@mit.edu>
2018-01-11 17:47:29 -0500

    Working Truncated Normal Mixture

--- em.py
+++ em.py
@@ -20,32 +20,10 @@ class Moments():
 		self.first_moment = self._first_moment()
 		self.second_moment = self._second_moment()
 
-	# def _first_moment(self):
-	# 	a_pdf = norm.pdf(self.a, loc=self.mu, scale=self.sigma)
-	# 	b_pdf = norm.pdf(self.b, loc=self.mu, scale=self.sigma)
-	# 	a_cdf = norm.cdf(self.a, loc=self.mu, scale=self.sigma)
-	# 	b_cdf = norm.cdf(self.b, loc=self.mu, scale=self.sigma)
-
-	# 	M1 = self.mu - self.sigma*(b_pdf-a_pdf)/(b_cdf-a_cdf)
-	# 	return M1
-
 	def _zeroth_moment(self):
 		upper_erf = erf((self.b-self.mu)/(self.sigma*sqrt(2)))
 		lower_erf = erf((self.a-self.mu)/(self.sigma*sqrt(2)))
 		return 0.5*(upper_erf - lower_erf)
-
-	# def _second_moment(self):
-	# 	a_pdf = norm.pdf(self.a, loc=self.mu, scale=self.sigma)
-	# 	b_pdf = norm.pdf(self.b, loc=self.mu, scale=self.sigma)
-	# 	a_pdf_der = -a_pdf*self.a
-	# 	b_pdf_der = -b_pdf*self.b
-	# 	a_cdf = norm.cdf(self.a, loc=self.mu, scale=self.sigma)
-	# 	b_cdf = norm.cdf(self.b, loc=self.mu, scale=self.sigma)
-
-	# 	derivative_term = (b_pdf_der - a_pdf_der)/(b_cdf-a_cdf)
-
-	# 	M2 = self.sigma**2 + self.mu**2 + self.sigma**2 * derivative_term - 2*self.mu*self.sigma*self.first_moment
-	# 	return M2
 
 	def _first_moment(self):
 		lower_exp = exp(-((self.a-self.mu)/(self.sigma*sqrt(2)))**2)
@@ -69,8 +47,6 @@ class TruncatedNormalDistribution(Distribution):
         self.b = upper
         self.ss_dist = truncnorm(a=(self.a-self.mu)/self.sigma, b=(self.b-self.mu)/self.sigma, \
         	loc=self.mu, scale=self.sigma)
-        # self.first_moment = Moments(self.mu, self.sigma, self.a, self.b).first_moment
-        # self.second_moment = Moments(self.mu, self.sigma, self.a, self.b).second_moment
 
     def log_density(self, data):
         assert(len(data.shape) == 1), "Expect 1D data!"
@@ -78,8 +54,9 @@ class TruncatedNormalDistribution(Distribution):
         lower_cdf = norm.cdf(self.a, loc=self.mu, scale=self.sigma)
         upper_cdf = 1 - norm.cdf(self.b, loc=self.mu, scale=self.sigma)
 
-        log_density = - ((data - self.mu) ** 2 / (2 * self.sigma ** 2) - np.log(self.sigma) - 0.5 * np.log(2 * np.pi))/(1-(lower_cdf+upper_cdf))
-
+        log_density = - ((data - self.mu) ** 2 / (2 * self.sigma ** 2) - np.log(self.sigma) \
+        	- 0.5 * np.log(2 * np.pi) - np.log(1-(lower_cdf+upper_cdf)))
+        log_density = np.where( np.logical_or(data < self.a, data > self.b) , -np.inf, log_density)
         return log_density
 
     def estimate_parameters(self, data, weights):
@@ -97,32 +74,3 @@ class TruncatedNormalDistribution(Distribution):
 
     def __repr__(self):
         return "TruncNorm[μ={mu:.4g}, σ={sigma:.4g}]".format(mu=self.mu, sigma=self.sigma)
-
-
-# if __name__ == "__main__":
-	# data1 = np.random.lognormal(mean=0, sigma=0.75, size=1000)
-	# data2 = np.random.lognormal(mean=10, sigma=0.9, size=1000)
-	# data3 = np.concatenate((data2, data1))
-	# print(data3)
-
-	# plt.hist(data3, normed=True, bins=1000)
-	# # plt.xlim(0, 1000)
-	# plt.show()
-
-	# mean_1, stdev_1 = norm.fit(data1)
-	# mean, std = norm.fit(data3)
-
-	# dist1 = mixem.distribution.LogNormalDistribution(0, 2)
-	# dist2 = mixem.distribution.LogNormalDistribution(11, 3)
-	# print(dist2.sigma)
-	# print(dist2.mu)
-
-	# weights, distributions, log_l = mixem.em(data3, [dist1, dist2], max_iterations=200)
-
-	# print("Weights: ", weights)
-	# print("Distributions: ", distributions)
-	# print("Log Likelihood: ", log_l)
-
-	# prob = mixem.probability(data3, weights, distributions)
-	# print("Probability: ", len(prob))
-
--- p_value.py
+++ p_value.py
@@ -10,7 +10,6 @@ class Data_Rep:
 	def __init__(self, data, dist_list):
 		self.cdf = self._mixture_cdf(data, dist_list)
 
-
 	def get_p_value(self, value):
 
 		p_value = 1 - self.cdf(value)
@@ -18,30 +17,22 @@ class Data_Rep:
 
 
 	def _mixture_cdf(self, data, dist_list):
-		# prob = mixem.probability(data, np.array([0.5, 0.5]), dist_list)
-		# print(sum(np.log(prob)))
-		weights, distributions, log_l = mixem.em(data, dist_list, progress_callback=None, initial_weights=[0.5, 0.5], max_iterations=200)
-		# prob = mixem.probability(data, weights, distributions)
-		# print(weights)
-		# print(log_l)
-		# print(sum(np.log(prob)))
-		scipy_dists = self._get_scipy_dists(distributions)
-		return lambda query: sum([w * dist.cdf(query) for w, dist in zip(weights, scipy_dists)])
+		weights, distributions, log_l = mixem.em(data, dist_list, progress_callback=None)
+		self.scipy_dists = self.get_scipy_dists(distributions)
+		return lambda query: sum([w * dist.cdf(query) for w, dist in zip(weights, self.scipy_dists)])
 
 
-	def _get_scipy_dists(self, distributions):
-
+	@staticmethod
+	def get_scipy_dists(distributions):
 		scipy_dists = []
 
 		for dist in distributions:
 			dist_type = type(dist)
-			# print(dist_type)
 
 			if dist_type == mixem.distribution.NormalDistribution:
 				loc = dist.mu
 				scale = dist.sigma
 				ss_dist = norm(loc=loc, scale=scale)
-				# print("here")
 				
 			elif dist_type == mixem.distribution.ExponentialDistribution:
 				l = dist.lmbda
@@ -52,50 +43,53 @@ class Data_Rep:
 				b = dist.b
 				loc = dist.mu
 				scale = dist.sigma
-				print(a, b, loc, scale)
-				ss_dist = truncnorm(a=(a-loc)/scale, b=(b-loc)/scale, loc=loc, scale=scale)
-				x = np.arange(-10, 10, 0.01)
-				plt.plot(ss_dist.pdf(x))
-				plt.show()
+				ss_dist = truncnorm(a=(a-loc)*1.0/scale, b=(b-loc)*1.0/scale, loc=loc, scale=scale)
 
 			scipy_dists.append(ss_dist)
 		return scipy_dists
 
 
-
 if __name__ == "__main__":
-
 
 	dist1 = TruncatedNormalDistribution(mu=0, sigma=1, lower=-1, upper=1)
 	dist2 = TruncatedNormalDistribution(mu=3, sigma=1, lower=2, upper=4)
 
-	predata1 = np.random.normal(loc=0, scale=1, size=100000)
+	predata1 = np.random.normal(loc=0, scale=1, size=1000000)
 	data1 = np.array(list(filter(lambda x: x > -1 and x < 1, predata1)))
 
-	predata2 = np.random.normal(loc=3, scale=1, size=100000)
+	predata2 = np.random.normal(loc=3, scale=1, size=1000000)
 	data2 = np.array(list(filter(lambda x: x > 2 and x < 4, predata2)))
 	
-	# dist1 = mixem.distribution.NormalDistribution(11, 3)
-	# data1 = np.random.normal(loc=10, scale=0.9, size=10000)
+	dist1 = mixem.distribution.NormalDistribution(11, 3)
+	data1 = np.random.normal(loc=10, scale=0.9, size=10000)
 
 
 	data = np.concatenate((data2, data1))
 	dist_list = [dist1, dist2]
+	scipy_dist_1, scipy_dist_2 = Data_Rep.get_scipy_dists(dist_list)
 	mixture = Data_Rep(data, dist_list)
-	scipy_dist_1, scipy_dist_2 = mixture._get_scipy_dists(dist_list)
-	# x = np.arange(-10, 10, 0.01)
-	# plt.plot(scipy_dist_2.pdf(x))
-	# # plt.hist(data, bins=1000)
-	# plt.show()
-	
+	post_scipy_dist_1, post_scipy_dist_2 = mixture.scipy_dists
 
-	# for i in [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
-	# # for i in [-10, -5, -1, -0.5, -0.25, 0, 0.25, 0.5, 1, 3, 5, 6, 7, 10]:
-	# 	print("VAL=", i)
-	# 	print("p-value of %s:" %i, mixture.get_p_value(i), "\n")
+	x = np.arange(
+		min(
+			-1.5, scipy_dist_1.ppf(0.001), scipy_dist_2.ppf(0.001),
+		    post_scipy_dist_1.ppf(0.001), post_scipy_dist_2.ppf(0.001)
+		),
+		max(
+			4.5, scipy_dist_1.ppf(0.999), scipy_dist_2.ppf(0.999),
+			post_scipy_dist_1.ppf(0.999), post_scipy_dist_2.ppf(0.999)
+		),
+		0.01
+	)
+	plt.plot(x, scipy_dist_1.pdf(x), label='-1 to 1 pdf')
+	plt.plot(x, scipy_dist_2.pdf(x), label='2 to 4 pdf')
+	plt.plot(x, post_scipy_dist_1.pdf(x), label= 'post %s to %s pdf' % (
+		post_scipy_dist_1.a, post_scipy_dist_1.b
+	))
+	plt.plot(x, post_scipy_dist_2.pdf(x), label= 'post %s to %s pdf' % (
+		post_scipy_dist_2.a, post_scipy_dist_2.b
+	))
 
-	# plt.hist(data, bins=100)
-	# rv = truncnorm(a=-1, b=1, loc=3)
-	# x = np.arange(0, 10, 0.01)
-	# plt.plot(x, rv.pdf(x))
-	# plt.show()
+	plt.hist(data, bins=200, normed=True, label='Real Data Histogram.')
+	plt.legend()
+	plt.show()
