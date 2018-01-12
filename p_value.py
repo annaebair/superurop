ca2ad5f81d3765b0213090dd198301dc6e92c493
Anna Bair <annabair@mit.edu>
2018-01-12 12:16:29 -0500

    Add Log Normal Distribution

--- p_value.py
+++ p_value.py
@@ -1,6 +1,7 @@
 import mixem
 import numpy as np
-from scipy.stats import norm, expon, truncnorm
+from math import exp
+from scipy.stats import norm, expon, truncnorm, lognorm
 import matplotlib.pyplot as plt
 from em import Moments, TruncatedNormalDistribution
 
@@ -39,57 +40,73 @@ class Data_Rep:
 				ss_dist = expon(l)
 
 			elif dist_type == TruncatedNormalDistribution:
-				a = dist.a
-				b = dist.b
 				loc = dist.mu
 				scale = dist.sigma
-				ss_dist = truncnorm(a=(a-loc)*1.0/scale, b=(b-loc)*1.0/scale, loc=loc, scale=scale)
+				a = float(dist.a-loc)/scale
+				b = float(dist.b-loc)/scale
+				ss_dist = truncnorm(a=a, b=b, loc=loc, scale=scale)
 
+			elif dist_type == mixem.distribution.LogNormalDistribution:
+				scale = exp(dist.mu)
+				s = dist.sigma
+				ss_dist = lognorm(s=s, scale=scale)
+				
 			scipy_dists.append(ss_dist)
 		return scipy_dists
 
 
 if __name__ == "__main__":
 
-	dist1 = TruncatedNormalDistribution(mu=0, sigma=1, lower=-1, upper=1)
-	dist2 = TruncatedNormalDistribution(mu=3, sigma=1, lower=2, upper=4)
+	# dist1 = TruncatedNormalDistribution(mu=0, sigma=1, lower=-1, upper=1)
+	# dist2 = TruncatedNormalDistribution(mu=3, sigma=1, lower=2, upper=4)
 
-	predata1 = np.random.normal(loc=0, scale=1, size=1000000)
-	data1 = np.array(list(filter(lambda x: x > -1 and x < 1, predata1)))
+	# predata1 = np.random.normal(loc=0, scale=1, size=1000000)
+	# data1 = np.array(list(filter(lambda x: x > -1 and x < 1, predata1)))
 
-	predata2 = np.random.normal(loc=3, scale=1, size=1000000)
-	data2 = np.array(list(filter(lambda x: x > 2 and x < 4, predata2)))
+	# predata2 = np.random.normal(loc=3, scale=1, size=1000000)
+	# data2 = np.array(list(filter(lambda x: x > 2 and x < 4, predata2)))
 	
-	dist1 = mixem.distribution.NormalDistribution(11, 3)
-	data1 = np.random.normal(loc=10, scale=0.9, size=10000)
+	# dist1 = mixem.distribution.NormalDistribution(11, 3)
+	# data1 = np.random.normal(loc=10, scale=0.9, size=10000)
 
+	dist1 = mixem.distribution.LogNormalDistribution(mu=3, sigma=1)
+	dist2 = mixem.distribution.LogNormalDistribution(mu=10, sigma=1)
+
+	data1 = np.random.lognormal(mean=3, sigma=1, size=1000)
+	data2 = np.random.lognormal(mean=10, sigma=1, size=1000)
 
 	data = np.concatenate((data2, data1))
 	dist_list = [dist1, dist2]
-	scipy_dist_1, scipy_dist_2 = Data_Rep.get_scipy_dists(dist_list)
+	scipy_dist_1,scipy_dist_2 = Data_Rep.get_scipy_dists(dist_list)
 	mixture = Data_Rep(data, dist_list)
 	post_scipy_dist_1, post_scipy_dist_2 = mixture.scipy_dists
 
-	x = np.arange(
-		min(
-			-1.5, scipy_dist_1.ppf(0.001), scipy_dist_2.ppf(0.001),
-		    post_scipy_dist_1.ppf(0.001), post_scipy_dist_2.ppf(0.001)
-		),
-		max(
-			4.5, scipy_dist_1.ppf(0.999), scipy_dist_2.ppf(0.999),
-			post_scipy_dist_1.ppf(0.999), post_scipy_dist_2.ppf(0.999)
-		),
-		0.01
-	)
-	plt.plot(x, scipy_dist_1.pdf(x), label='-1 to 1 pdf')
-	plt.plot(x, scipy_dist_2.pdf(x), label='2 to 4 pdf')
-	plt.plot(x, post_scipy_dist_1.pdf(x), label= 'post %s to %s pdf' % (
-		post_scipy_dist_1.a, post_scipy_dist_1.b
-	))
-	plt.plot(x, post_scipy_dist_2.pdf(x), label= 'post %s to %s pdf' % (
-		post_scipy_dist_2.a, post_scipy_dist_2.b
-	))
 
-	plt.hist(data, bins=200, normed=True, label='Real Data Histogram.')
-	plt.legend()
+	# x = np.arange(
+	# 	min(
+	# 		-1.5, scipy_dist_1.ppf(0.001), scipy_dist_2.ppf(0.001),
+	# 	    post_scipy_dist_1.ppf(0.001), post_scipy_dist_2.ppf(0.001)
+	# 	),
+	# 	max(
+	# 		4.5, scipy_dist_1.ppf(0.999), scipy_dist_2.ppf(0.999),
+	# 		post_scipy_dist_1.ppf(0.999), post_scipy_dist_2.ppf(0.999)
+	# 	),
+	# 	0.01
+	# )
+
+	x = np.arange(0, 100000, 0.01)
+
+	plt.subplot(2, 1, 1)
+	plt.hist(data1, bins=100, normed=True)
+	plt.plot(x, post_scipy_dist_1.pdf(x))
+	plt.xlim(0, 600)
+	plt.title("Distribution 1")
+
+	plt.subplot(2, 1, 2)
+	plt.hist(data2, bins=300, normed=True)
+	plt.plot(x, post_scipy_dist_2.pdf(x))
+	plt.xlim(0, 50000)
+	plt.title("Distribution 2")
+	plt.tight_layout()
+
 	plt.show()
