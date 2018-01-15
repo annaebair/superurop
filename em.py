b03905b46b067c5c05c809a33e5a632dfb6232bd
Anna Bair <annabair@mit.edu>
2018-01-15 11:24:31 -0500

    censored dist WIP

--- em.py
+++ em.py
@@ -56,21 +56,75 @@ class TruncatedNormalDistribution(Distribution):
 
         log_density = - ((data - self.mu) ** 2 / (2 * self.sigma ** 2) - np.log(self.sigma) \
         	- 0.5 * np.log(2 * np.pi) - np.log(1-(lower_cdf+upper_cdf)))
-        log_density = np.where( np.logical_or(data < self.a, data > self.b) , -np.inf, log_density)
+        log_density = np.where(np.logical_or(data < self.a, data > self.b) , -np.inf, log_density)
         return log_density
 
     def estimate_parameters(self, data, weights):
         assert(len(data.shape) == 1), "Expect 1D data!"
 
         wsum = np.sum(weights)
-
+        # print("weights: ", weights)
+        # print("length of weights: ", len(weights))
+        # print("wsum: ", wsum)
+        # print("data length: ", len(data))
         moments = Moments(0, self.sigma, self.a-self.mu, self.b-self.mu)
         m_k = moments.first_moment
         H_k = self.sigma - moments.second_moment
 
         self.mu = np.sum(weights * data) / wsum - m_k
+        print((data - self.mu) ** 2)
         self.sigma = np.sqrt(np.sum(weights * (data - self.mu) ** 2) / wsum + H_k)
-
+        print(self.sigma)
 
     def __repr__(self):
         return "TruncNorm[μ={mu:.4g}, σ={sigma:.4g}]".format(mu=self.mu, sigma=self.sigma)
+
+
+class CensoredNormalDistribution(Distribution):
+    """Censored normal distribution with parameters (mu, sigma)."""
+
+    def __init__(self, mu, sigma, lower, upper):
+        self.mu = mu
+        self.sigma = sigma
+        self.a = lower
+        self.b = upper
+
+    def log_density(self, data):
+        assert(len(data.shape) == 1), "Expect 1D data!"
+        return - (data - self.mu) ** 2 / (2 * self.sigma ** 2) - np.log(self.sigma) - 0.5 * np.log(2 * np.pi)
+
+    def estimate_parameters(self, data, weights):
+        assert(len(data.shape) == 1), "Expect 1D data!"
+
+        wsum = np.sum(weights)
+
+
+        moments = Moments(self.mu, self.sigma, self.a-self.mu, self.b-self.mu)
+        first_moment = moments.first_moment
+        second_moment = moments.second_moment
+        weights = np.array(weights)[:, np.newaxis]
+
+        new_data = np.where(np.logical_or(data < self.a, data > self.b) , first_moment, data)
+        new_data = new_data[:, np.newaxis]
+        self.mu = np.sum(np.multiply(new_data, weights)) / wsum
+        print("mu: ", self.mu)
+        
+        term = new_data - self.mu
+        pre_S = np.dot(term, term.T)
+        R = second_moment - first_moment ** 2
+        R_matrix = np.zeros(pre_S.shape)
+        R_matrix[-1][-1] = R
+        S = pre_S + R_matrix
+        self.sigma = np.sum(np.dot(S, weights)) / wsum
+
+
+    def __repr__(self):
+        return "CensoredNorm[μ={mu:.4g}, σ={sigma:.4g}]".format(mu=self.mu, sigma=self.sigma)
+
+if __name__ == "__main__":
+    dist = CensoredNormalDistribution(0, 1, -1, 1)
+    data = np.random.normal(loc=0, scale=1.0, size=10000)
+    moments = Moments(0, 1, -1, 1)
+    weights = np.ones(10000)
+    dist.estimate_parameters(data, weights)
+
--- p_value.py
+++ p_value.py
@@ -3,7 +3,22 @@ import numpy as np
 from math import exp
 from scipy.stats import norm, expon, truncnorm, lognorm
 import matplotlib.pyplot as plt
-from em import Moments, TruncatedNormalDistribution
+from em import Moments, TruncatedNormalDistribution, CensoredNormalDistribution
+
+class censorednorm:
+	def __init__(self, loc, scale, a, b):
+		self.loc = loc
+		self.scale = scale
+		self.a = a
+		self.b = b
+
+	def cdf(self, query):
+		if query < self.a:
+			return 0
+		elif query > self.b:
+			return 1
+		else:
+			return scipy.stats.norm(loc=self.loc, scale=self.scale)
 
 
 class Data_Rep:
@@ -18,7 +33,7 @@ class Data_Rep:
 
 
 	def _mixture_cdf(self, data, dist_list):
-		weights, distributions, log_l = mixem.em(data, dist_list, progress_callback=None)
+		weights, distributions, log_l = mixem.em(data, dist_list)
 		self.scipy_dists = self.get_scipy_dists(distributions)
 		return lambda query: sum([w * dist.cdf(query) for w, dist in zip(weights, self.scipy_dists)])
 
@@ -50,36 +65,49 @@ class Data_Rep:
 				scale = exp(dist.mu)
 				s = dist.sigma
 				ss_dist = lognorm(s=s, scale=scale)
-				
+
+			elif dist_type == CensoredNormalDistribution:
+				loc = dist.mu
+				scale = dist.sigma
+				a = dist.a
+				b = dist.b
+				ss_dist = censorednorm(loc, scale, a, b)
+
 			scipy_dists.append(ss_dist)
 		return scipy_dists
 
 
 if __name__ == "__main__":
 
-	# dist1 = TruncatedNormalDistribution(mu=0, sigma=1, lower=-1, upper=1)
-	# dist2 = TruncatedNormalDistribution(mu=3, sigma=1, lower=2, upper=4)
+	dist1 = CensoredNormalDistribution(mu=0, sigma=1, lower=-1, upper=1)
+	dist2 = CensoredNormalDistribution(mu=3, sigma=1, lower=2, upper=4)
 
-	# predata1 = np.random.normal(loc=0, scale=1, size=1000000)
+	predata1 = np.random.normal(loc=0, scale=1, size=1000)
 	# data1 = np.array(list(filter(lambda x: x > -1 and x < 1, predata1)))
+	nextdata1 = np.where(predata1 >= -1, predata1, -1)
+	data1 = np.where(nextdata1 <= 1, predata1, 1)
 
-	# predata2 = np.random.normal(loc=3, scale=1, size=1000000)
+	predata2 = np.random.normal(loc=3, scale=1, size=1000)
+	nextdata2 = np.where(predata1 >= 2, predata1, 2)
+	data2 = np.where(nextdata1 <= 4, predata1, 4)
 	# data2 = np.array(list(filter(lambda x: x > 2 and x < 4, predata2)))
 	
 	# dist1 = mixem.distribution.NormalDistribution(11, 3)
 	# data1 = np.random.normal(loc=10, scale=0.9, size=10000)
 
-	dist1 = mixem.distribution.LogNormalDistribution(mu=3, sigma=1)
-	dist2 = mixem.distribution.LogNormalDistribution(mu=10, sigma=1)
+	# dist1 = mixem.distribution.LogNormalDistribution(mu=3, sigma=1)
+	# dist2 = mixem.distribution.LogNormalDistribution(mu=10, sigma=1)
 
-	data1 = np.random.lognormal(mean=3, sigma=1, size=1000)
-	data2 = np.random.lognormal(mean=10, sigma=1, size=1000)
+	# data1 = np.random.lognormal(mean=3, sigma=1, size=1000)
+	# data2 = np.random.lognormal(mean=10, sigma=1, size=1000)
 
-	data = np.concatenate((data2, data1))
+	data = np.concatenate((data1, data2))
 	dist_list = [dist1, dist2]
 	scipy_dist_1,scipy_dist_2 = Data_Rep.get_scipy_dists(dist_list)
+
 	mixture = Data_Rep(data, dist_list)
 	post_scipy_dist_1, post_scipy_dist_2 = mixture.scipy_dists
+
 
 
 	# x = np.arange(
@@ -94,19 +122,19 @@ if __name__ == "__main__":
 	# 	0.01
 	# )
 
-	x = np.arange(0, 100000, 0.01)
+	# x = np.arange(0, 100000, 0.01)
 
-	plt.subplot(2, 1, 1)
-	plt.hist(data1, bins=100, normed=True)
-	plt.plot(x, post_scipy_dist_1.pdf(x))
-	plt.xlim(0, 600)
-	plt.title("Distribution 1")
+	# plt.subplot(2, 1, 1)
+	# plt.hist(data1, bins=100, normed=True)
+	# plt.plot(x, post_scipy_dist_1.pdf(x))
+	# plt.xlim(0, 600)
+	# plt.title("Distribution 1")
 
-	plt.subplot(2, 1, 2)
-	plt.hist(data2, bins=300, normed=True)
-	plt.plot(x, post_scipy_dist_2.pdf(x))
-	plt.xlim(0, 50000)
-	plt.title("Distribution 2")
-	plt.tight_layout()
+	# plt.subplot(2, 1, 2)
+	# plt.hist(data2, bins=300, normed=True)
+	# plt.plot(x, post_scipy_dist_2.pdf(x))
+	# plt.xlim(0, 50000)
+	# plt.title("Distribution 2")
+	# plt.tight_layout()
 
-	plt.show()
+	# plt.show()
