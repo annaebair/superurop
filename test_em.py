c40ac503146d6fdae06ec6f87eec9e72d3f978a3
Anna Bair <annabair@mit.edu>
2018-02-01 13:59:32 -0500

    Update truncated log density tests

--- test_em.py
+++ test_em.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.stats as s
 from math import isclose
 from em import Moments, TruncatedNormalDistribution, CensoredNormalDistribution
+from mixem.distribution import NormalDistribution
 from p_value import Data_Rep
 
 class TestEM:
@@ -77,17 +78,45 @@ class TestEM:
 
 	################ Truncated log_density #################
 
-	def test_log_density_trunc_norm(self):
-		mu = 4
-		sigma = 2
-		a = 3
-		b = 6
-		data = np.array([2, 3, 6, 7])
-		dist = TruncatedNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
-		actual = dist.log_density(data)
-		expected = np.array([-9999, -9999, -9999, -9999])
-		assert actual.all() == expected.all()
+	def test_trunc_log_density(self):
+		mu=0
+		sigma=1
+		a=-1
+		b=1
+		data = np.array([-0.5, 0, 0.5])
+		expected_dist = s.truncnorm(loc=mu, scale=sigma, a=a, b=b)
+		actual_dist = TruncatedNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
+		expected = expected_dist.logpdf(data)
+		actual = actual_dist.log_density(data)
+		assert (actual == expected).all()
 
+	def test_trunc_log_density_shifted(self):
+		mu=3
+		sigma=2
+		a=0
+		b=4
+		data = np.array([0.5, 1, 2, 3, 3.9])
+		expected_dist = s.truncnorm(loc=mu, scale=sigma, a=(a-mu)/sigma, b=(b-mu)/sigma)
+		actual_dist = TruncatedNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
+		expected = expected_dist.logpdf(data)
+		actual = actual_dist.log_density(data)
+		for i in range(len(expected)):
+			assert isclose(actual[i], expected[i], abs_tol=1e-10)
+
+	def test_trunc_log_density_outside_range(self):
+		mu=3
+		sigma=2
+		a=0
+		b=4
+		data = np.array([-1, 1, 2, 3, 5])
+		expected_dist = s.truncnorm(loc=mu, scale=sigma, a=(a-mu)/sigma, b=(b-mu)/sigma)
+		actual_dist = TruncatedNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
+		expected = expected_dist.logpdf(data)
+		expected[0] = -9999
+		expected[-1] = -9999
+		actual = actual_dist.log_density(data)
+		for i in range(len(expected)):
+			assert isclose(actual[i], expected[i], abs_tol=1e-10)
 
 	################ Censored log_density #################
 
@@ -100,7 +129,7 @@ class TestEM:
 		dist = CensoredNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
 		actual = dist.log_density(data)
 		expected = np.array([-9999, -9999])
-		assert actual.all() == expected.all()
+		assert (actual == expected).all()
 
 	def test_log_density_cens_norm_at_bounds(self):
 		mu = 4
@@ -111,6 +140,18 @@ class TestEM:
 		dist = CensoredNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
 		actual = dist.log_density(data)
 		assert all(i > 1 for i in actual)
+
+	def test_compare_censored_to_normal(self):
+		mu = 4
+		sigma = 2
+		a = 3
+		b = 6
+		data = np.array([3.5, 4, 5, 5.5])
+		dist = CensoredNormalDistribution(mu=mu, sigma=sigma, lower=a, upper=b)
+		norm = s.norm(loc=mu, scale=sigma)
+		actual = dist.log_density(data)
+		expected = norm.logpdf(data)
+		assert (actual == expected).all()
 
 
 	################ Truncated estimate_parameters #################	
@@ -188,6 +229,8 @@ class TestEM:
 
 		dist1 = CensoredNormalDistribution(mu=0.5, sigma=1, lower=0, upper=1)
 		dist2 = CensoredNormalDistribution(mu=1.5, sigma=1, lower=2, upper=4)
+
+		# after dataRep, make sure that params are close to expected
 		
 		data = np.concatenate((data1, data2))
 		dist_list = [dist1, dist2]
@@ -222,3 +265,34 @@ class TestEM:
 
 		for i in range(len(expected)):
 			assert isclose(expected[i], observed[i], abs_tol=1e-8)
+
+
+
+	# def test_censored_and_normal_p_values(self):
+	# 	data1 = np.random.normal(loc=0, scale=1, size=1000)
+	# 	data1 = np.where(data1 <= 0, 0, data1)
+	# 	data1 = np.where(data1 >= 1, 1, data1)
+
+	# 	data2 = np.random.normal(loc=3, scale=1, size=1000)
+
+	# 	dist1 = CensoredNormalDistribution(mu=0.5, sigma=1, lower=0, upper=1)
+	# 	dist2 = NormalDistribution(mu=3, sigma=1)
+
+	# 	# after dataRep, make sure that params are close to expected
+		
+	# 	data = np.concatenate((data1, data2))
+	# 	dist_list = [dist1, dist2]
+	# 	mixture = Data_Rep(data, dist_list)
+	# 	dist_weight = mixture.weights[0]
+	# 	print("sf: ", s.norm.sf(-1.5))
+
+	# 	observed = []
+	# 	for i in [-1, 1.5, 5]:
+	# 		observed.append(mixture.get_p_value(i))
+	# 	expected = [0+s.norm.sf(-4), 1-(dist_weight+s.norm.cdf(-1.5)), 0+ s.norm.sf(2)] #manually get p value here
+
+	# 	#get p values of normal
+	# 	print(observed)
+
+	# 	for i in range(len(expected)):
+	# 		assert isclose(expected[i], observed[i], abs_tol=1e-4)
