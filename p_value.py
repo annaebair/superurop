3174d1d9d2dd26037d9268e7b1bbeb14d7f004a3
Anna Bair <annabair@mit.edu>
2018-02-01 13:59:02 -0500

    updated code for p_value

--- p_value.py
+++ p_value.py
@@ -48,9 +48,7 @@ class Data_Rep:
 		return p_value
 
 	def _mixture_cdf(self, data, dist_list):
-		self.weights, self.distributions, self.log_l = mixem.em(data, dist_list, max_iterations=500, progress_callback=None)
-		# print("probability: ", sum(mixem.probability(data, np.array([0.5, 0.5]), dist_list)))
-		# print("probability: ", sum(mixem.probability(data, self.weights, self.distributions)))
+		self.weights, self.distributions, self.log_l = mixem.em(data, dist_list, max_iterations=200, progress_callback=None)
 		# print("distributions: ", self.distributions)
 		# print("weights: ", self.weights)
 		# print("log_l: ", self.log_l)
@@ -98,36 +96,64 @@ class Data_Rep:
 
 if __name__ == "__main__":
 
-	# Censored
+	# Censored 1
 	predata1 = np.random.normal(loc=0, scale=1, size=1000)
-	data1 = np.where(predata1 <= -3, -3, predata1)
-	data1 = np.where(data1 >= 4, 4, data1)
-	dist1 = CensoredNormalDistribution(mu=0, sigma=1, lower=-3, upper=4)
+	censored_data_1 = np.where(predata1 <= -1, -1, predata1)
+	censored_data_1 = np.where(censored_data_1 >= 3, 3, censored_data_1)
+	censored_dist_1 = CensoredNormalDistribution(mu=1.0, sigma=1, lower=-1, upper=3)
 
-	# Censored
-	predata4 = np.random.normal(loc=3, scale=1, size=1000)
-	data4 = np.where(predata4 <= -3, -3, predata4)
-	data4 = np.where(data4 >= 4, 4, data4)
-	dist4 = CensoredNormalDistribution(mu=3, sigma=1, lower=-3, upper=4)
+	# Censored 2
+	predata2 = np.random.normal(loc=3, scale=1, size=1000)
+	censored_data_2 = np.where(predata2 <= -3, -3, predata2)
+	censored_data_2 = np.where(censored_data_2 >= 4, 4, censored_data_2)
+	censored_dist_2 = CensoredNormalDistribution(mu=1.5, sigma=1, lower=-3, upper=4)
 
-	# Truncated
-	predata2 = np.random.normal(loc=0, scale=1, size=1447)
-	data2 = np.array(list(filter(lambda x: x > -1 and x < 3, predata2)))
-	dist2 = TruncatedNormalDistribution(mu=0.5, sigma=1, lower=-1, upper=3)
+	# Truncated 1
+	predata3 = np.random.normal(loc=1, scale=1, size=100000)
+	truncated_data_1 = np.array(list(filter(lambda x: x > 0 and x < 2, predata3)))
+	truncated_dist_1 = TruncatedNormalDistribution(mu=1.106, sigma=1, lower=0, upper=2)
 
-	# Truncated
-	predata5 = np.random.normal(loc=2, scale=1, size=1547)
-	data5 = np.array(list(filter(lambda x: x > 0 and x < 4, predata5)))
-	dist5 = TruncatedNormalDistribution(mu=2, sigma=1, lower=0, upper=4)
+	# Truncated 2
+	predata4 = np.random.normal(loc=5, scale=1, size=146500)
+	truncated_data_2 = np.array(list(filter(lambda x: x > 4 and x < 6, predata4)))
+	truncated_dist_2 = TruncatedNormalDistribution(mu=5, sigma=1.004, lower=4, upper=6)
 
-	# Normal
-	data3 = np.random.normal(loc=2, scale=1, size=1000)
-	dist3 = mixem.distribution.NormalDistribution(mu=0.5, sigma=1)
-	
-	data = np.concatenate((data1, data4))
-	dist_list = [dist1, dist4]
+	# Normal 1
+	normal_data_1 = np.random.normal(loc=0.5, scale=1, size=100000)
+	normal_dist_1 = mixem.distribution.NormalDistribution(mu=0.5, sigma=1)
+
+	# Normal 2
+	normal_data_2 = np.random.normal(loc=3, scale=2, size=100000)
+	normal_dist_2 = mixem.distribution.NormalDistribution(mu=3.434, sigma=1.964)
+
+	trunced_normal = np.array(list(filter(lambda x: x > 0 and x < 2, normal_data_2)))
+	trunced_normal_dist = TruncatedNormalDistribution(mu=3, sigma=2, lower=0, upper=2)
+
+	# print("trunced normal mean: ", sum(trunced_normal)/len(trunced_normal))
+
+	data = np.concatenate((normal_data_2, truncated_data_1))
+	np.random.shuffle(data)
+	val_set = data[:1000]
+	train_set = data[1000:]
+	# print("val length: ", len(val_set))
+	# print("train length: ", len(train_set))
+	# print("trunc len: ", len(truncated_data_1))
+
+	pre_scipy_dist_1 = norm(3, 2)
+	pre_scipy_dist_2 = truncnorm(a=-1, b=1, loc=1, scale=1)
+
+	dist_list = [normal_dist_2, truncated_dist_1]
 	mixture = Data_Rep(data, dist_list)
-	post_scipy_dist_1, post_scipy_dist_3 = mixture.scipy_dists
+	post_scipy_dist_1, post_scipy_dist_2 = mixture.scipy_dists
+	weight_normal = len(normal_data_2)/len(data)
+	weight_trunc = 1-weight_normal
+	pre_weights = np.array([weight_normal, weight_trunc])
+
+	sorted_val = np.array(sorted(val_set))
+
+	weights, distributions, log_l = mixem.em(train_set, dist_list, max_iterations=200, progress_callback=None)
+	post_prob = mixem.probability(sorted_val, weights, distributions)
+
 
 	def find_best_initialization(data1, data2):
 		best_log_l = -np.inf
@@ -152,22 +178,57 @@ if __name__ == "__main__":
 		print("Best mus: ", best_mus)
 		print("Best dists: ", best_dists)
 
-	find_best_initialization(data1, data4)
-
 	x = np.arange(-7, 7, 0.001)
 
-	pdf = [post_scipy_dist_1.pdf(i) for i in x]
-	pdf_3 = [post_scipy_dist_3.pdf(i) for i in x]
+	pdf1 = [post_scipy_dist_1.pdf(i) for i in x]
+	pdf2 = [post_scipy_dist_2.pdf(i) for i in x]
+	prepdf1 = [pre_scipy_dist_1.pdf(i) for i in x]
+	prepdf2 = [pre_scipy_dist_2.pdf(i) for i in x]
 
-	for i in [-1, 0, 0.5, 1, 1.5, 3, 4, 4.5, 5, 6, 7]:
-		query = i
-		p_val = mixture.get_p_value(query)
-		# print("p value of %s: " % query, p_val)
 
-	plt.hist(data1, bins=100, normed=True)
-	plt.hist(data4, bins=100, normed=True)
+	def get_p_values():
+		for i in [-1, 0, 0.5, 1, 1.5, 3, 4, 4.5, 5, 6, 7]:
+			query = i
+			p_val = mixture.get_p_value(query)
+			print("p value of %s: " % query, p_val)
 
-	plt.plot(x, pdf)
-	plt.plot(x, pdf_3)
-	plt.ylim(0, 6)
+	# plt.plot(val_set, pre_prob, 'bo')
+	# plt.plot(val_set, post_prob, 'go')
+	# plt.show()
+	
+	plt.figure
+	plt.subplot(221)
+	plt.hist(truncated_data_1, bins=100, normed=True)
+	plt.hist(normal_data_2, bins=100, normed=True)
+	plt.plot(x, pdf1, label="post1")
+	plt.plot(x, pdf2, label="post2")
+	plt.plot(x, prepdf1, label="pre1")
+	plt.plot(x, prepdf2, label="pre2")
+	plt.ylim(0, 1)
+	plt.xlim(-5, 10)
+	plt.legend()
+	# plt.show()
+
+	plt.subplot(222)
+	plt.hist(data, bins=200, normed=True)
+	plt.plot(x, pdf1, label="post1")
+	plt.plot(x, pdf2, label="post2")
+	plt.plot(x, prepdf1, label="pre1")
+	plt.plot(x, prepdf2, label="pre2")
+	plt.ylim(0, 1)
+	plt.xlim(-5, 10)
+
+	plt.subplot(223)
+	d = np.concatenate((truncated_data_1, trunced_normal))
+	plt.hist(d, bins=100, normed=True)
+	plt.ylim(0, 1)
+	plt.xlim(0, 2)
+
+
+	plt.subplot(224)
+	generated = np.random.normal(loc=1.112, scale=1.004, size=100000)
+	generated_trunc = np.array(list(filter(lambda x: x > 0 and x < 2, generated)))
+	plt.hist(generated_trunc, bins=100, normed=True)
+	plt.ylim(0, 1)
+	plt.xlim(0, 2)
 	plt.show()
